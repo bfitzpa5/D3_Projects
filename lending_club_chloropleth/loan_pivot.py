d5777a72137636192453bfe3598fa6cf2f23d6ad
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2019-11-08 21:22:11 -0500

    Update loan_pivot to swap default and fully paid

--- lending_club_chloropleth/loan_pivot.py
+++ lending_club_chloropleth/loan_pivot.py
@@ -11,15 +11,15 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-
 def loan_status_filter(input_df):
     df = input_df.copy()
     loan_status_lst = [
             'Fully Paid',
             'Charged Off',
-            'Late (16-30 days)',
-            'Late (31-120 days)',
-            'Default']
+            #'Late (16-30 days)',
+            #'Late (31-120 days)',
+            'Default',
+    ]
     mask = df.loan_status.isin(loan_status_lst)
     return df.loc[mask, :]
 
@@ -30,26 +30,28 @@ def state_abbr_table():
     table = str(soup.find('table', class_="stripedRows"))
     return pd.read_html(table)[0]
 
+def dc_filter(input_df):
+    df = input_df.copy()
+    return df.loc[df['US State'] != 'District of Columbia', :]
+
 fname = os.path.join(
-    os.path.expanduser('~'),
-    r'\Documents\GitHub\Metis_Projects\03-mcnulty\Data\loan.csv'
+    os.path.expanduser('~'), 'Documents', 'GitHub', 'Metis_Projects', 
+    '03-mcnulty', 'Data', 'loan.csv'
 )
 df = (pd.read_csv(fname)
     .pipe(loan_status_filter)
-    .assign(default=lambda x: np.where(x.loan_status=='Fully Paid', 'Default', 'Fully Paid'))
+    .assign(default=lambda x: np.where(x.loan_status=='Fully Paid', 'fully_paid', 'default'))
     .loc[:, ['addr_state', 'default']])
 
 df_states = (state_abbr_table()
     .rename(columns=lambda x: x.replace(':', ''))
     .set_index('Abbreviation'))
 
-def dc_filter(input_df):
-    df = input_df.copy()
-    return df.loc[df['US State'] != 'District of Columbia', :]
-
-(df.pivot_table(index='addr_state', columns='default', aggfunc=len)
+(df.pivot_table(index='addr_state', columns='default', aggfunc=len, fill_value=0)
  .join(df_states, how='left')
- .assign(default_rate=lambda x: x['Default'].div(x['Fully Paid'] + x['Default']))
- .loc[:, ['US State', 'default_rate']]
+ .assign(total_loans=lambda x: x['fully_paid'] + x['default'],
+         default_rate=lambda x: x['default'].div(x['total_loans']))
  .pipe(dc_filter)
+ .rename(columns={'US State': 'state'})
+ .set_index('state')
  .to_csv(r'Data/default_rates_by_state.csv'))
